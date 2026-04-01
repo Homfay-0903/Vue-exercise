eeb14c30baea5b4ac4aaee37a3ce0232f0a057e9
Homfay-0903 <2790165595@qq.com>
2026-04-01 20:37:01 +0800

    Axios-4.1

--- Core/Ts-Axios/src/hooks/useDebounce.ts
+++ Core/Ts-Axios/src/hooks/useDebounce.ts
@@ -1,7 +1,7 @@
 import { ref, watch, onUnmounted } from 'vue'
 
 export const useDebounce = <T>(value: T, delay: number = 500) => {
-    const debounceValue = ref(value) as { value: T }
+    const debounceValue = ref<T>(value)
     let timer: number | null = null
 
     watch(
@@ -22,4 +22,21 @@ export const useDebounce = <T>(value: T, delay: number = 500) => {
             clearTimeout(timer)
         }
     })
+}
+
+export const debounce = <T extends (...args: any[]) => void>(
+    fn: T,
+    delay: number = 500
+): (...args: Parameters<T>) => void => {
+    let timer: number | null = null
+
+    return (...args: Parameters<T>) => {
+        if (timer) {
+            clearTimeout(timer)
+        }
+
+        timer = setTimeout(() => {
+            fn(...args)
+        }, delay)
+    }
 }
