3d9624ebe28b774ee5d2999fb84ad7e496569300
Homfay-0903 <2790165595@qq.com>
2026-04-21 18:58:56 +0800

    Component-button

---
+++ Core/Ts-Component/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Button type="default">1</Button>
+    <Button type="primary">2</Button>
+    <Button type="success">3</Button>
+    <Button type="danger">4</Button>
+    <Button>5</Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from './components/Button.vue';
+</script>
+
+<style scoped></style>
---
+++ Core/Ts-Component/src/components/Button.vue
@@ -0,0 +1,53 @@
+<template>
+    <button class="general-style" :class="aloneStyle">
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+//interface Button {
+//    type: string
+//}
+
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'default',
+        validator: (value: string) => {
+            return ['default', 'primary', 'success', 'danger'].includes(value)
+        }
+    }
+})
+
+const aloneStyle = computed(() => {
+    return `alone-style-${props.type}`
+})
+</script>
+
+<style scoped>
+.general-style {
+    padding: 6px 16px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 14px;
+}
+
+.alone-style-default {
+    background-color: black;
+}
+
+.alone-style-primary {
+    background-color: blue;
+}
+
+.alone-style-success {
+    background-color: green;
+}
+
+.alone-style-danger {
+    background-color: red;
+}
+</style>
