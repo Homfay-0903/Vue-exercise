d859494f4cf3e89b3430ed3df099849401d2d220
Homfay-0903 <2790165595@qq.com>
2026-04-21 19:23:24 +0800

    Component-input

--- Core/Ts-Component/src/App.vue
+++ Core/Ts-Component/src/App.vue
@@ -6,10 +6,17 @@
     <Button type="danger">4</Button>
     <Button>5</Button>
   </div>
+  <div>
+    <Input v-model="value" clearable></Input>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import Button from './components/Button.vue';
+import Input from './components/Input.vue';
+
+const value = ref('')
 </script>
 
 <style scoped></style>
---
+++ Core/Ts-Component/src/components/Input.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <input type="text" v-model="inputValue" :placeholder="placeholder" :disabled="disabled">
+        <span v-if="inputValue && clearable && !disabled" @click="handleClear">x</span>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+interface Input {
+    modelValue: string
+    clearable?: boolean
+    disabled?: boolean
+    placeholder?: string
+}
+
+const props = withDefaults(defineProps<Input>(), {
+    clearable: false,
+    disabled: false,
+    placeholder: 'please input'
+})
+
+const emits = defineEmits<{
+    'update:modelValue': [value: string]
+}>()
+
+const inputValue = computed({
+    get() {
+        return props.modelValue
+    },
+    set(value) {
+        emits('update:modelValue', value)
+    }
+})
+
+const handleClear = () => {
+    emits('update:modelValue', '')
+}
+</script>
+
+<style scoped></style>
